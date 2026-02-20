51fd799ed65560c5de6f9093326bf9eaea4b9502
naveenpandey <naveenppandey8080@gmail.com>
2026-02-20 21:03:03 +0530

    BFSTraversal

--- src/Tree/BFSTraversal.java
+++ src/Tree/BFSTraversal.java
@@ -1,53 +1,58 @@
 package Tree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class BFSTraversal {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-    class Node {
-        int data;
-        Node left, right;
-
-        public Node(int data) {
-            this.data = data;
+        public TreeNode(int val) {
+            this.val = val;
             left = right = null;
         }
     }
-
-    Node root;
-
-    // BFS Traversal (Level Order)
-    void BFS(Node node) {
-
-        if (node == null) return;
-
-        Queue<Node> q = new LinkedList<>();
-        q.add(node);
-
+    public static List<List<Integer>> BFS(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
         while (!q.isEmpty()) {
-            Node current = q.poll();
-            System.out.print(current.data + " ");
-
-            if (current.left != null) {
-                q.add(current.left);
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode current = q.poll();
+                level.add(current.val);
+                if (current.left != null) {
+                    q.offer(current.left);
+                }
+                if (current.right != null) {
+                    q.offer(current.right);
+                }
             }
-
-            if (current.right != null) {
-                q.add(current.right);
-            }
+            result.add(level);
         }
+        return result;
     }
-
     public static void main(String[] args) {
         BFSTraversal tree = new BFSTraversal();
 
-        tree.root = tree.new Node(1);
-        tree.root.left = tree.new Node(2);
-        tree.root.right = tree.new Node(3);
-        tree.root.left.left = tree.new Node(4);
-        tree.root.left.right = tree.new Node(5);
+        TreeNode root = tree.new TreeNode(1);
+        root.left = tree.new TreeNode(2);
+        root.right = tree.new TreeNode(3);
+        root.left.left = tree.new TreeNode(4);
+        root.left.right = tree.new TreeNode(5);
+        root.right.left = tree.new TreeNode(6);
+        root.right.right = tree.new TreeNode(7);
 
-        tree.BFS(tree.root);
+        List<List<Integer>> result = BFS(root);
+
+        for (List<Integer> level : result) {
+            System.out.println(level);
+        }
     }
 }
